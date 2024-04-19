3de1078735f719af649acdbc6bc867e957eb2003
Матвей Петров <matveygemot@gmail.com>
2024-04-19 22:29:49 +0700

    port

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ mongoose
   .then(() => console.log("DB is OK"))
   .catch((err) => console.log("DB is not OK", err));
 
-const PORT = process.env.PORT || 4444;
+const PORT = 4444;
 
 const app = express();
 
