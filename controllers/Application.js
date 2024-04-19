42522b286495d07accd3f9ef3c6fe5856772fa11
Матвей Петров <matveygemot@gmail.com>
2024-04-19 22:03:22 +0700

    city error

--- controllers/Application.js
+++ controllers/Application.js
@@ -1,52 +1,59 @@
 const nodemailer = require("nodemailer");
 const { validationResult } = require("express-validator");
 const ApplicationModel = require("../models/Application.js");
-const dotenv = require("dotenv");
-
-dotenv.config();
 
 const application = async (req, res) => {
-  const result = validationResult(req);
-  if (!result.isEmpty()) {
-    return res.json(result.array());
-  }
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.APP_GMAIL,
-      pass: process.env.APP_PASS,
-    },
-  });
-  const mailOptions = {
-    from: process.env.APP_GMAIL,
-    to: process.env.APP_GMAIL,
-    subject: "VLASOV PRODUCTION",
-    text: `Имя: ${req.body.name}
-Телефон: ${req.body.phone}
-Сообщение: ${req.body.text}
-Город: ${req.body.city}`,
-  };
-
-  const doc = new ApplicationModel({
-    name: req.body.name,
-    phone: req.body.phone,
-    text: req.body.text,
-    city: req.body.city,
-  });
-
-  await doc.save();
-
-  transporter.sendMail(mailOptions, (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("nodemailer is OK");
+  try {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.json(result.array());
     }
-  });
+    if (req.body.city !== "Выберите город") {
+      const transporter = nodemailer.createTransport({
+        service: "mail",
+        auth: {
+          user: process.env.APP_GMAIL,
+          pass: process.env.APP_PASS,
+        },
+      });
+      const mailOptions = {
+        from: process.env.APP_GMAIL,
+        to: process.env.APP_GMAIL,
+        subject: "VLASOV PRODUCTION",
+        text: `Имя: ${req.body.name}
+		Телефон: ${req.body.phone}
+		Сообщение: ${req.body.text}
+		Город: ${req.body.city}`,
+      };
 
-  res.json({
-    message: "success",
-  });
+      const doc = new ApplicationModel({
+        name: req.body.name,
+        phone: req.body.phone,
+        text: req.body.text,
+        city: req.body.city,
+      });
+
+      await doc.save();
+
+      transporter.sendMail(mailOptions, (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("nodemailer is OK");
+        }
+      });
+
+      res.json({
+        message: "success",
+      });
+    } else {
+      res.json({
+        message: "error",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 module.exports = application;
